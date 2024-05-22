d11c966a5b83d06c88a03f22f476b7e07cbf0900
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-05-22 17:33:58 -0500

    chore: initial commit.

---
+++ src/main/java/com/acme/hormonalcare/backend/HormonalCareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.hormonalcare.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HormonalCareBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HormonalCareBackendApplication.class, args);
+    }
+
+}
