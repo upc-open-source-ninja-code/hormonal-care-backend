c5b3bd3e33301d6075f73c0b2d4a5d25fb5170bd
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-05-28 10:16:52 -0500

    fix: third try to fix initial structure.

--- src/main/java/com/acme/hormonalcare/backend/learning/domain/model/commands/UpdateStudentMetricsOnTutorialCompletedCommand.java
+++ src/main/java/com/acme/hormonalcare/backend/learning/domain/model/commands/UpdateStudentMetricsOnTutorialCompletedCommand.java
@@ -1,6 +1,6 @@
 package com.acme.hormonalcare.backend.learning.domain.model.commands;
 
-import com.acme.center.platform.learning.domain.model.valueobjects.AcmeStudentRecordId;
+import com.acme.hormonalcare.backend.learning.domain.model.valueobjects.AcmeStudentRecordId;
 
 public record UpdateStudentMetricsOnTutorialCompletedCommand(AcmeStudentRecordId studentRecordId) {
 }
