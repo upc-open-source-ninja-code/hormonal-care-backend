2bdfd22bfbcd92bd69b437972541cfa227df84e0
Luis Sebastian Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-05-30 08:19:31 -0500

    fix(Medication aggregate root and ReasonOfConsultation aggregate root) - fixed to upper cammel case

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/aggregates/Medication.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/aggregates/Medication.java
@@ -2,6 +2,6 @@ package com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates;
 
 import com.acme.hormonalcare.backend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
 
-public class medication extends AuditableAbstractAggregateRoot<medication> {
+public class Medication extends AuditableAbstractAggregateRoot<Medication> {
 
 }
