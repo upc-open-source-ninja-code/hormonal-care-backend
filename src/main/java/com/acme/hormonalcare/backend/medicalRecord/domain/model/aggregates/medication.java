5dd5cccad1c4d34b57f7acb7df37adcdea8e892e
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-05-30 07:51:28 -0500

    Chore: added initial structure to medical-record.

---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/aggregates/medication.java
@@ -0,0 +1,7 @@
+package com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates;
+
+import com.acme.hormonalcare.backend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+
+public class medication extends AuditableAbstractAggregateRoot<medication> {
+
+}
---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/interfaces/rest/transform/prueba.java
@@ -0,0 +1,4 @@
+package com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.transform;
+
+public class prueba {
+}
