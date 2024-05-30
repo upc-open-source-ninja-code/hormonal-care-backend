87ab16bc7510a6aa1f83783f5275abb3f0c697db
Luis Sebastian Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-05-30 08:40:29 -0500

    feat(ReasonOfConsultation): added class aggregate
    added atributes and constructor functions

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/aggregates/ReasonOfConsultation.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/aggregates/ReasonOfConsultation.java
@@ -1,4 +1,22 @@
 package com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates;
 
-public class ReasonOfConsultation {
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.CreateReasonOfConsultationCommand;
+import com.acme.hormonalcare.backend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+
+public class ReasonOfConsultation extends AuditableAbstractAggregateRoot<ReasonOfConsultation> {
+    private String description;
+    private String symptoms;
+
+    public ReasonOfConsultation() {
+    }
+
+    public ReasonOfConsultation(String description, String symptoms) {
+        this.description = description;
+        this.symptoms = symptoms;
+    }
+
+    public ReasonOfConsultation (CreateReasonOfConsultationCommand command) {
+        this.description = command.description();
+        this.symptoms = command.symptoms();
+    }
 }
