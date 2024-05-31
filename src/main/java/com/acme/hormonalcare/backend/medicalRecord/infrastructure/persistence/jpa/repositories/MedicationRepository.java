09e2068e3ad6770f9f70b899de7fd2982a141b5f
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-05-31 13:36:22 -0500

    feat(medical-record-medication): Added MedicationRepository in infrstructure layer.

---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/infrastructure/persistence/jpa/repositories/MedicationRepository.java
@@ -0,0 +1,27 @@
+package com.acme.hormonalcare.backend.medicalRecord.infrastructure.persistence.jpa.repositories;
+
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.Medication;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.valueobjects.DrugName;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MedicationRepository extends JpaRepository<Medication, Long> {
+
+    Optional<Medication> findByDrugName(DrugName drugName);
+    boolean existsMedicationByDrugName(DrugName drugName);
+
+   //List<Medication> findByMedicalRecordId(Long medicalRecordId);
+    //boolean existsMedicationByMedicalRecordId(Long medicalRecordId);
+
+    List<Medication> findByPrescriptionId(Long prescriptionId);
+    boolean existsMedicationByPrescriptionId(Long prescriptionId);
+
+    List<Medication> findByMedicationTypeId(Long typeId);
+    boolean existsMedicationByMedicationTypeId(Long typeId);
+
+}
