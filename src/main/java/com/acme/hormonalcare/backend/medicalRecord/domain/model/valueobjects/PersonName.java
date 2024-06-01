b59b30e57ff1c416e931c7b675a6a16c7fb0aa8d
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-01 18:27:20 -0500

    doc: functionality of the Value Object created.

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/valueobjects/PersonName.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/valueobjects/PersonName.java
@@ -1,4 +1,18 @@
 package com.acme.hormonalcare.backend.medicalRecord.domain.model.valueobjects;
 
-public record PersonName() {
+public record PersonName(String firstName, String lastName) {
+
+    public PersonName() { this(null, null); }
+
+    public PersonName {
+        if(firstName == null || firstName.isBlank()) {
+            throw new IllegalArgumentException("First name must not be null or blank");
+        }
+        if(lastName == null || lastName.isBlank()) {
+            throw new IllegalArgumentException("Last name must not be null or blank");
+        }
+    }
+    public String getFullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
 }
