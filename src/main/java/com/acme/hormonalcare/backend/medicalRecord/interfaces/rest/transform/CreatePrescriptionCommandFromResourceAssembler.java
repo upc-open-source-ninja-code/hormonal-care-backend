89b95340f33f90dad548042e9ce771de7e2eddf7
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-05-31 21:25:24 -0500

    feat(medical-record-medication): Added CreatePrescriptionCommandFromResourceAssembler.

---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/interfaces/rest/transform/CreatePrescriptionCommandFromResourceAssembler.java
@@ -0,0 +1,16 @@
+package com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.transform;
+
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.CreatePrescriptionCommand;
+import com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.resources.CreatePrescriptionResource;
+
+public class CreatePrescriptionCommandFromResourceAssembler {
+    public static CreatePrescriptionCommand toCommandFromResource(CreatePrescriptionResource resource) {
+        return new CreatePrescriptionCommand(
+                resource.medicalRecord(),
+                resource.prescriptionDate(),
+                resource.doctorId(),
+                resource.patientId(),
+                resource.notes()
+        );
+    }
+}
