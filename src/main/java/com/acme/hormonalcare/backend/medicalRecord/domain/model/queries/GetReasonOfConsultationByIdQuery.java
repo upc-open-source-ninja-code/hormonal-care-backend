d162be91cad5581d6d76ca484d216e4a9054d6e1
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-06-02 17:33:04 -0500

    feat(GetReasonOfConsultationByIdQuery): added id parameter to the query

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/queries/GetReasonOfConsultationByIdQuery.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/queries/GetReasonOfConsultationByIdQuery.java
@@ -1,4 +1,4 @@
 package com.acme.hormonalcare.backend.medicalRecord.domain.model.queries;
 
-public record GetReasonOfConsultationByIdQuery() {
+public record GetReasonOfConsultationByIdQuery(Long id) {
 }
