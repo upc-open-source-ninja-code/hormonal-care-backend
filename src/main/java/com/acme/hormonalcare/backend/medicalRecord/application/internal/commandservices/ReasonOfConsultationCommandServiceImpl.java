510fa632da790223c36518c78e7fdabbd43e50d7
Luis Sebastián Chinchihualpa Saldarriaga <u202212112@upc.edu.pe>
2024-06-02 17:27:23 -0500

    feat(ReasonOfConsultationCommandServiceImpl): added implementation for handle the update method and fixed the create method

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/application/internal/commandservices/ReasonOfConsultationCommandServiceImpl.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/application/internal/commandservices/ReasonOfConsultationCommandServiceImpl.java
@@ -2,6 +2,7 @@ package com.acme.hormonalcare.backend.medicalRecord.application.internal.command
 
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.ReasonOfConsultation;
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.CreateReasonOfConsultationCommand;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.UpdateReasonOfConsultationCommand;
 import com.acme.hormonalcare.backend.medicalRecord.domain.services.ReasonOfConsultationCommandService;
 import com.acme.hormonalcare.backend.medicalRecord.infrastructure.persistence.jpa.repositories.ReasonOfConsultationRepository;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,23 @@ public class ReasonOfConsultationCommandServiceImpl implements ReasonOfConsultat
     public Optional<ReasonOfConsultation> handle(CreateReasonOfConsultationCommand command) {
         var reasonOfConsultation = new ReasonOfConsultation(command);
         reasonOfConsultationRepository.save(reasonOfConsultation);
-        return Optional.empty();
+        return Optional.of(reasonOfConsultation);
+    }
+
+    @Override
+    public Optional<ReasonOfConsultation> handle(UpdateReasonOfConsultationCommand command) {
+        var id = command.id();
+        if (!reasonOfConsultationRepository.existsById(id)) {
+            throw new IllegalArgumentException("ReasonOfConsultation with id "+ command.id() +"does not exists");
+        }
+        var result = reasonOfConsultationRepository.findById(id);
+        var reasonOfConsultationToUpdate = result.get();
+        try {
+            var updatedReasonOfConsultation = reasonOfConsultationRepository.save(reasonOfConsultationToUpdate.updateInformation(command.description(),command.symptoms()));
+            return Optional.of(updatedReasonOfConsultation);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error while updating reasonOfConsultation: " + e.getMessage());
+        }
+
     }
 }
