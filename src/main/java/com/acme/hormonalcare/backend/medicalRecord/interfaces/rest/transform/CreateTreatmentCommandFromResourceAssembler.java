f9d2edb5f65932b9da16d3265d7dec8c171ae9f7
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-02 22:30:57 -0500

    Feat(CreateTreatmentCommandFromResourceAssembler)
    creating CreateTreatmentCommandFromResourceAssembler

---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/interfaces/rest/transform/CreateTreatmentCommandFromResourceAssembler.java
@@ -0,0 +1,10 @@
+package com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.transform;
+
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.CreateTreatmentCommand;
+import com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.resources.CreateTreatmentResource;
+
+public class CreateTreatmentCommandFromResourceAssembler {
+    public static CreateTreatmentCommand toCommandFromResource(CreateTreatmentResource resource){
+        return new CreateTreatmentCommand(resource.description());
+    }
+}
