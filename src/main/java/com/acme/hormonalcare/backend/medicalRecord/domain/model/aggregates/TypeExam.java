97f0a5cf0d912a764a7a6d3e5b46c911af100dcd
Estefano-Lostaunau <u202211742@upc.edu.pe>
2024-06-04 08:42:10 -0500

    feat(TypeExam): modify file

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/aggregates/TypeExam.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/aggregates/TypeExam.java
@@ -1,4 +1,20 @@
 package com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.CreateTypeExamCommand;
+import com.acme.hormonalcare.backend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Entity;
+import lombok.Getter;
 
-public class TypeExam {
+@Getter
+@Entity
+public class TypeExam extends AuditableAbstractAggregateRoot<ReasonOfConsultation> {
+    private String name;
+
+    public TypeExam() {
+    }
+    public TypeExam(String name) {
+        this.name = name;
+    }
+    public TypeExam(CreateTypeExamCommand command) {
+        this.name = command.name();
+    }
 }
