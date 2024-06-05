3d6fa0ca9a75d35f0381ba63f5c5802f20fefaae
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-05 08:32:03 -0500

    Feat(PersonName, Email, Gender, Age, Birthday)
    creating value objects PersonName, Email, Gender, Age, Birthday

---
+++ src/main/java/com/acme/hormonalcare/backend/profile/domain/model/valueobjects/Gender.java
@@ -0,0 +1,16 @@
+package com.acme.hormonalcare.backend.profile.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public record Gender(String gender) {
+    public Gender() { this(null); }
+    public Gender{
+        if (gender == null || gender.isBlank()) {
+            throw new IllegalArgumentException("Gender cannot be null or empty");
+        }
+        if (!gender.equals("Male") && !gender.equals("Female")) {
+            throw new IllegalArgumentException("Gender must be 'Male' or 'Female'");
+        }
+    }
+}
