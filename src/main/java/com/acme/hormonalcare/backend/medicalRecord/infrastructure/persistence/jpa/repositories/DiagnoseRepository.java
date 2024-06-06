9ec63fece8a971fcc110f1fcc6caf0915e9d981a
Seba <sebalobato05@gmail.com>
2024-06-06 16:17:12 -0500

    Create DiagnoseRepository.java

---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/infrastructure/persistence/jpa/repositories/DiagnoseRepository.java
@@ -0,0 +1,14 @@
+package com.acme.hormonalcare.backend.medicalRecord.infrastructure.persistence.jpa.repositories;
+
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.Diagnose;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DiagnoseRepository extends JpaRepository<Diagnose, Long> {
+
+    Optional<Diagnose> findByDiagnose(String diagnose);
+    boolean existsByDiagnose(String diagnose);
+}
