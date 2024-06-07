845609d0de546d7f932ba31106028da5901ccead
JHS-Sama24 <112771173+JHS-Sama24@users.noreply.github.com>
2024-06-06 21:43:54 -0500

    feat(deploy): added configuration

--- src/main/java/com/acme/hormonalcare/backend/HormonalCareBackendApplication.java
+++ src/main/java/com/acme/hormonalcare/backend/HormonalCareBackendApplication.java
@@ -2,7 +2,11 @@ package com.acme.hormonalcare.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,8 +16,6 @@ public class HormonalCareBackendApplication {
         SpringApplication.run(HormonalCareBackendApplication.class, args);
     }
 
-}
-    /*
     @Configuration
     public static class Myconfiguration{
         @Bean
@@ -26,4 +28,5 @@ public class HormonalCareBackendApplication {
                 }
             };
         }
-    }*/
+    }
+}
