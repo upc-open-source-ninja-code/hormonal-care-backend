24c70f16977a3794b45cd49e19ca14884d094f43
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-07 08:40:14 -0500

    feat(DoctorQueryServiceImpl)
    creating DoctorQueryServiceImpl

---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/application/internal/queryservices/DoctorQueryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.acme.hormonalcare.backend.medicalRecord.application.internal.queryservices;
+
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.Doctor;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetAllDoctorsQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByIdQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.services.DoctorQueryService;
+import com.acme.hormonalcare.backend.medicalRecord.infrastructure.persistence.jpa.repositories.DoctorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorQueryServiceImpl implements DoctorQueryService {
+    private final DoctorRepository doctorRepository;
+
+    public DoctorQueryServiceImpl(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
+    @Override
+    public Optional<Doctor> handle(GetDoctorByIdQuery query) {
+        return doctorRepository.findById(query.id());
+    }
+
+    @Override
+    public List<Doctor> handle(GetAllDoctorsQuery query) {
+        return doctorRepository.findAll();
+    }
+
+}
