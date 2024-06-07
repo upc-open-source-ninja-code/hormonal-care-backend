31195fbb735ebbb4c8d1e931d6ed03861b1c75d7
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-07 08:45:39 -0500

    feat(DoctorController)
    creating DoctorController

---
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/interfaces/rest/DoctorController.java
@@ -0,0 +1,63 @@
+package com.acme.hormonalcare.backend.medicalRecord.interfaces.rest;
+
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.UpdateDoctorSubscriptionCommand;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.commands.UpdateDoctorAppointmentFeeCommand;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByIdQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.services.DoctorCommandService;
+import com.acme.hormonalcare.backend.medicalRecord.domain.services.DoctorQueryService;
+import com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.resources.CreateDoctorResource;
+import com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.resources.DoctorResource;
+import com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.transform.CreateDoctorCommandFromResourceAssembler;
+import com.acme.hormonalcare.backend.medicalRecord.interfaces.rest.transform.DoctorResourceFromEntityAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/doctor/doctor", produces = MediaType.APPLICATION_JSON_VALUE)
+public class DoctorController {
+    private final DoctorCommandService doctorCommandService;
+    private final DoctorQueryService doctorQueryService;
+
+    public DoctorController(DoctorCommandService doctorCommandService, DoctorQueryService doctorQueryService) {
+        this.doctorCommandService = doctorCommandService;
+        this.doctorQueryService = doctorQueryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<DoctorResource> createDoctor(@RequestBody CreateDoctorResource resource){
+        var createDoctorCommand = CreateDoctorCommandFromResourceAssembler.toCommandFromResource(resource);
+        var doctor = doctorCommandService.handle(createDoctorCommand);
+        if(doctor.isEmpty()) return ResponseEntity.badRequest().build();
+        var doctorResource = DoctorResourceFromEntityAssembler.toResourceFromEntity(doctor.get());
+        return new ResponseEntity<>(doctorResource, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{doctorId}")
+    public ResponseEntity<DoctorResource> getDoctorById(@PathVariable Long doctorId){
+        var getDoctorByIdQuery = new GetDoctorByIdQuery(doctorId);
+        var doctor = doctorQueryService.handle(getDoctorByIdQuery);
+        if(doctor.isEmpty()) return ResponseEntity.notFound().build();
+        var doctorResource = DoctorResourceFromEntityAssembler.toResourceFromEntity(doctor.get());
+        return ResponseEntity.ok(doctorResource);
+    }
+
+    @PutMapping("/{doctorId}/subscription")
+    public ResponseEntity<DoctorResource> updateDoctorSubscription(@PathVariable Long doctorId, @RequestBody String subscription){
+        var updateDoctorSubscriptionCommand = new UpdateDoctorSubscriptionCommand(doctorId, subscription);
+        var updatedDoctor = doctorCommandService.handle(updateDoctorSubscriptionCommand);
+        if(updatedDoctor.isEmpty()) return ResponseEntity.notFound().build();
+        var doctorResource = DoctorResourceFromEntityAssembler.toResourceFromEntity(updatedDoctor.get());
+        return ResponseEntity.ok(doctorResource);
+    }
+
+    @PutMapping("/{doctorId}/appointmentFee")
+    public ResponseEntity<DoctorResource> updateDoctorAppointmentFee(@PathVariable Long doctorId, @RequestBody String appointmentFee){
+        var updateDoctorAppointmentFeeCommand = new UpdateDoctorAppointmentFeeCommand(doctorId, appointmentFee);
+        var updatedDoctor = doctorCommandService.handle(updateDoctorAppointmentFeeCommand);
+        if(updatedDoctor.isEmpty()) return ResponseEntity.notFound().build();
+        var doctorResource = DoctorResourceFromEntityAssembler.toResourceFromEntity(updatedDoctor.get());
+        return ResponseEntity.ok(doctorResource);
+    }
+}
