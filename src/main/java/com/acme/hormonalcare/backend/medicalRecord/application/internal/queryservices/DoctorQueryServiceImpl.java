58b28b72c7db8fed0f8bb2b9fc9fd2a9d3e01d4d
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-20 11:57:49 -0500

    fix(DoctorQueryServiceImpl)
    fixing DoctorQueryServiceImpl

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/application/internal/queryservices/DoctorQueryServiceImpl.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/application/internal/queryservices/DoctorQueryServiceImpl.java
@@ -2,7 +2,9 @@ package com.acme.hormonalcare.backend.medicalRecord.application.internal.queryse
 
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.Doctor;
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetAllDoctorsQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByDoctorRecordIdQuery;
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByIdQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByProfileIdQuery;
 import com.acme.hormonalcare.backend.medicalRecord.domain.services.DoctorQueryService;
 import com.acme.hormonalcare.backend.medicalRecord.infrastructure.persistence.jpa.repositories.DoctorRepository;
 import org.springframework.stereotype.Service;
@@ -20,12 +22,23 @@ public class DoctorQueryServiceImpl implements DoctorQueryService {
 
     @Override
     public Optional<Doctor> handle(GetDoctorByIdQuery query) {
+        
         return doctorRepository.findById(query.id());
     }
 
     @Override
     public List<Doctor> handle(GetAllDoctorsQuery query) {
         return doctorRepository.findAll();
+    }
+
+    @Override
+    public Optional<Doctor> handle(GetDoctorByDoctorRecordIdQuery query) {
+        return doctorRepository.findByDoctorRecordId(query.doctorRecordId());
+    }
+
+    @Override
+    public Optional<Doctor> handle(GetDoctorByProfileIdQuery query) {
+        return doctorRepository.findByProfileId(query.profileId());
     }
 
 }
