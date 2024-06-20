58a8923acb1d9382c8768fc01006f051a54788e7
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-20 11:58:36 -0500

    fix(doctor commands)
    fixing doctor commands

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/commands/UpdateDoctorSubscriptionCommand.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/model/commands/UpdateDoctorSubscriptionCommand.java
@@ -2,6 +2,6 @@ package com.acme.hormonalcare.backend.medicalRecord.domain.model.commands;
 
 public record UpdateDoctorSubscriptionCommand(
         Long id,
-        String subscriptionId
+        Long subscriptionId
 ) {
 }
