8bfdee5bd0798c703e7afa4af8dc23dbe0ba9cd7
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-20 11:59:00 -0500

    fix(Doctor services)
    fixing doctor services

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/services/DoctorQueryService.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/domain/services/DoctorQueryService.java
@@ -2,7 +2,9 @@ package com.acme.hormonalcare.backend.medicalRecord.domain.services;
 
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.Doctor;
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetAllDoctorsQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByDoctorRecordIdQuery;
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByIdQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetDoctorByProfileIdQuery;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +12,6 @@ import java.util.Optional;
 public interface DoctorQueryService {
     Optional<Doctor> handle(GetDoctorByIdQuery query);
     List<Doctor> handle(GetAllDoctorsQuery query);
+    Optional<Doctor> handle(GetDoctorByDoctorRecordIdQuery query);
+    Optional<Doctor> handle(GetDoctorByProfileIdQuery query);
 }
