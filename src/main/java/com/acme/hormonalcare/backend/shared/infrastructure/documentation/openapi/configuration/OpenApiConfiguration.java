290f64adedde12ebf98d874848752921043240c8
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-22 17:52:51 -0500

    Fix(OpenApiCOnfiguration)
    stop IAM

--- src/main/java/com/acme/hormonalcare/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/acme/hormonalcare/backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -26,7 +26,7 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("ACME hormonal Care Wiki Documentation")
                         .url("https://acme-hormonal-care.wiki.github.io/docs"));
-        // Add Security Scheme
+        /* Add Security Scheme
         final String securitySchemeName = "bearerAuth";
         openApi.addSecurityItem(new SecurityRequirement()
                         .addList(securitySchemeName))
@@ -37,7 +37,7 @@ public class OpenApiConfiguration {
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
                                         .bearerFormat("JWT")));
-        // Return OpenAPI configuration object
+        // Return OpenAPI configuration object*/
         return openApi;
     }
 }
