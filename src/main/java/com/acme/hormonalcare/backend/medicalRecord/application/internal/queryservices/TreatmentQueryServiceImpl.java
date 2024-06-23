64db4b69e0cf873b8d53c32f1c6e9793d5e42ec2
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-23 18:15:01 -0500

    fix(TreatmentQueryServiceImpl)
    including get by medicalrecordid

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/application/internal/queryservices/TreatmentQueryServiceImpl.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/application/internal/queryservices/TreatmentQueryServiceImpl.java
@@ -2,6 +2,7 @@ package com.acme.hormonalcare.backend.medicalRecord.application.internal.queryse
 
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.Treatment;
 import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetTreatmentByIdQuery;
+import com.acme.hormonalcare.backend.medicalRecord.domain.model.queries.GetTreatmentByMedicalRecordIdQuery;
 import com.acme.hormonalcare.backend.medicalRecord.domain.services.TreatmentQueryService;
 import com.acme.hormonalcare.backend.medicalRecord.infrastructure.persistence.jpa.repositories.TreatmentRepository;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,10 @@ public class TreatmentQueryServiceImpl implements TreatmentQueryService {
     @Override
     public Optional<Treatment> handle(GetTreatmentByIdQuery query) {
         return treatmentRepository.findById(query.id());
+    }
+
+    @Override
+    public Optional<Treatment> handle(GetTreatmentByMedicalRecordIdQuery query) {
+        return treatmentRepository.findByMedicalRecordId(query.medicalRecordId());
     }
 }
