4fc690f83c14935028f7f43841c2e5a9e0392fd7
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2024-06-23 18:16:16 -0500

    fix(TreatmentRepository)
    including get by medicalrecordid

--- src/main/java/com/acme/hormonalcare/backend/medicalRecord/infrastructure/persistence/jpa/repositories/TreatmentRepository.java
+++ src/main/java/com/acme/hormonalcare/backend/medicalRecord/infrastructure/persistence/jpa/repositories/TreatmentRepository.java
@@ -4,6 +4,9 @@ import com.acme.hormonalcare.backend.medicalRecord.domain.model.aggregates.Treat
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TreatmentRepository extends JpaRepository<Treatment, Long> {
+    Optional<Treatment> findByMedicalRecordId(Long medicalRecordId);
 }
